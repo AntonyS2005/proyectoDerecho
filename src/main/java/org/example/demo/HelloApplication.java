af63d4ee615a086f9cde5b9d4df7c4b115a48547
Antony <santoslopeztony@gmail.com>
2024-04-17 18:10:09 -0600

    agregue los estilos en css

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -3,15 +3,19 @@ package org.example.demo;
   import javafx.application.Application;
   import javafx.fxml.FXMLLoader;
   import javafx.scene.Scene;
+  import javafx.scene.text.Font;
   import javafx.stage.Stage;
 
   import java.io.IOException;
 
   public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menuPrincipal.fxml"));
       Scene scene = new Scene(fxmlLoader.load());
+      String css = this.getClass().getResource("menu.css").toExternalForm();
+      scene.getStylesheets().add(css);
     stage.setTitle("Iniciar secion");
     stage.setScene(scene);
     stage.show();
